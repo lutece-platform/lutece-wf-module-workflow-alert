855f71f4b9d881c10c128a24fff9473f5b4ec33e
vbroussard <vbroussard@users.noreply.github.com>
2014-07-28 09:59:31 +0000

    WORKFLOWALERT-11 : Correction of SQL creation script.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/directory-form-workflow/module-workflow-alert@70054 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/workflow/modules/alert/plugin/create_db_workflow_alert.sql
+++ src/sql/plugins/workflow/modules/alert/plugin/create_db_workflow_alert.sql
@@ -3,11 +3,11 @@
 --
 DROP TABLE IF EXISTS task_alert_cf;
 CREATE TABLE task_alert_cf(
-  id_task INT(11) DEFAULT 0 NOT NULL,
-  id_directory INT(11) DEFAULT 0 NOT NULL,
-  id_state_after_deadline INT(11) DEFAULT 0 NOT NULL,
-  position_directory_entry_date INT(11) DEFAULT 0 NOT NULL,
-  nb_days_to_date INT(11) DEFAULT 0 NOT NULL,
+  id_task INT DEFAULT 0 NOT NULL,
+  id_directory INT DEFAULT 0 NOT NULL,
+  id_state_after_deadline INT DEFAULT 0 NOT NULL,
+  position_directory_entry_date INT DEFAULT 0 NOT NULL,
+  nb_days_to_date INT DEFAULT 0 NOT NULL,
   id_retrieval_type SMALLINT DEFAULT 0 NOT NULL,
   PRIMARY KEY (id_task)
 );
@@ -17,8 +17,8 @@ CREATE TABLE task_alert_cf(
 --
 DROP TABLE IF EXISTS task_alert;
 CREATE TABLE task_alert(
-  id_history INT(11) DEFAULT 0 NOT NULL,
-  id_task INT(11) DEFAULT 0 NOT NULL,
+  id_history INT DEFAULT 0 NOT NULL,
+  id_task INT DEFAULT 0 NOT NULL,
   reference_date TIMESTAMP DEFAULT NULL NULL,
   PRIMARY KEY (id_history, id_task)
 );
